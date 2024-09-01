d1a10735d9b8c04e498643982999f2109239a2fb
Khuat Quoc Khanh <23711381.khanh@student.iuh.edu.vn>
2024-09-01 17:20:32 +0700

    bai 3

---
+++ src/iuh/cntt/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"iuh.cntt.khdl","l":"Course"},{"p":"iuh.cntt.khdl","l":"CourseList"},{"p":"<Unnamed>","l":"Main"},{"p":"iuh.cntt.khdl","l":"TestCourse"}];updateSearchResults();
