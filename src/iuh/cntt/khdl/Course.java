c696726b7250e9e941f045a6f91879435d486923
Khuat Quoc Khanh <23711381.khanh@student.iuh.edu.vn>
2024-09-01 17:16:48 +0700

    https://github.com/KhanhQuoc0203/Bai3.git

---
+++ src/iuh/cntt/khdl/Course.java
@@ -0,0 +1,82 @@
+/*
+ * @ (#) Course.java   1.0 25/08/2024
+ *
+ *  copyright (c) 2024 IUH. All rights reserved.
+ */
+package iuh.cntt.khdl;
+
+/*
+ * @description:
+ * @author: Khanh Quoc
+ * @date : 25/08/2024
+ * @version: 1.0
+ */
+public class Course {
+
+    private String id;
+    private String title;
+    private int credit;
+    private String department;
+
+    public Course()
+    {
+        this("", "",0, "");
+    }
+
+    public Course(String id, String title, int credit, String department) {
+        this.setId(id);
+        this.id = id;
+        this.title = title;
+        this.credit = credit;
+        this.department = department;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(String id) {
+
+        if(id.trim().length() < 3)
+            throw new IllegalArgumentException("Length of ID is at least 3");
+        for (int i = 0; i < id.length(); i++) {
+            char ch = id.charAt(i);
+            if(!Character.isLetterOrDigit(ch))
+                throw new IllegalArgumentException("ID contains only letters or numbers");
+        }
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        if(title.isEmpty())
+            throw new IllegalArgumentException("Cannot be left blank");
+        this.title = title;
+    }
+
+    public void setCredit(int credit) {
+        if(credit < 0)
+            throw new IllegalArgumentException("Credit must be greater than 0");
+        this.credit = credit;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-10s%-25s%10d %-10s", id, title, credit, department);
+    }
+
+}
+
---
+++ src/iuh/cntt/khdl/TestCourse.java
@@ -0,0 +1,66 @@
+/*
+ * @ (#) TestCourse.java   1.0 25/08/2024
+ *
+ *  copyright (c) 2024 IUH. All rights reserved.
+ */
+package iuh.cntt.khdl;
+import java.util.Scanner;
+/*
+ * @description:
+ * @author: Khanh Quoc
+ * @date : 25/08/2024
+ * @version: 1.0
+ */
+public class TestCourse {
+    public static void main(String[] args) {
+        Course c1 = new Course("IT202", "Lap Trinh C", 2, "CS");
+        Course c2 = new Course("FR393", "Co So Du Lieu", 3, "SE");
+        Course c3 = new Course("CR876", "Ky Thuat Lap Trinh", 1, "CS");
+        Course c4 = new Course("IT192", "Khoa Hoc May Tinh", 5, "SE");
+        Course c5 = new Course("FR621", "Toan Roi Rac", 4, "CS");
+
+        CourseList courseList = new CourseList(10);
+        courseList.addCourse(c1);
+        courseList.addCourse(c2);
+        courseList.addCourse(c3);
+        courseList.addCourse(c4);
+        courseList.addCourse(c5);
+        for (Course x : courseList.getCourses()) {
+            if(x!=null)
+                System.out.println(x);
+        }
+        System.out.println("The department with the most course:\n" + courseList.findDepartmenWithMostCourse());
+
+
+        courseList.removeCourse("IT192");
+
+        System.out.println("List of courses after removing\n");
+        for (Course x : courseList.getCourses())
+        {
+            if(x!=null)
+                System.out.println(x);
+        }
+
+        System.out.println("\nCourse with the most credits: ");
+        for (Course c : courseList.findMaxCreditCourse())
+        {
+            if(c!=null)
+                System.out.println(c);
+        }
+
+        System.out.println("\nSearch for courses by title");
+        courseList.searchCourse("Co So Du Lieu");
+
+        System.out.println("\nFind courses by department:");
+        courseList.searchCourseByDepartment("SE");
+
+        System.out.println("\nFind course by ID");
+        System.out.println(courseList.searchCourseByID("IT202"));
+        System.out.println("\nList of Course after sorting");
+        for (Course x : courseList.sortCourse())
+        {
+            if(x!=null)
+                System.out.println(x);
+        }
+    }
+}
